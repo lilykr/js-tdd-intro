8f8a18d3935c8acf350cd04f4cb4dc2cbc1f6779
lilykr <lisa-lou.k@hotmail.fr>
2020-12-15 16:22:55 +0100

    added separate test file

--- capitalizeFirstLetters.js
+++ capitalizeFirstLetters.js
@@ -7,17 +7,3 @@ function capitalizeFirstLetters(string) {
   : '';
 }
 
-// Check that capitalizeFirstLetters is a function
-assert.strictEqual(typeof capitalizeFirstLetters, 'function');
-
-// Check that capitalizeFirst accepts one argument
-assert.strictEqual(capitalizeFirstLetters.length, 1);
-
-// Check that capitalizeFirst transforms javaScript correctly
-assert.strictEqual(capitalizeFirstLetters('i am bold'), 'I Am Bold');
-
-// Check that it works for a 1-character string
-assert.strictEqual(capitalizeFirstLetters('z'), 'Z');
-
-// Check that it works for an empty string
-assert.strictEqual(capitalizeFirstLetters(''), '');
---
+++ test/test/capitalizeFirst.test.js
@@ -0,0 +1,19 @@
+describe('capitalizeFirst', () => {
+  // give the test a label using it
+  it('is a function accepting one argument', () => {
+    assert.strictEqual(typeof capitalizeFirstLetters, 'function');
+    assert.strictEqual(capitalizeFirstLetters.length, 1);
+  });
+
+  it('transforms i am bold correctly', () => {
+    assert.strictEqual(capitalizeFirstLetters('i am bold'), 'I Am Bold');
+  });
+
+  it('works with a 1-character string', () => {
+    assert.strictEqual(capitalizeFirstLetters('z'), 'Z');
+  });
+
+  it('works with an empty string', () => {
+    assert.strictEqual(capitalizeFirstLetters(''), '');
+  });
+});
